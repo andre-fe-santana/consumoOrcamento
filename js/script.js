7872abf74a71b96dd3d6c9a40b77d1db4ddb2a8a
Andre <castroferreiraandre@gmail.com>
2026-08-17 10:17:53 -0300

    feat: Criando funcao de copiar texto

--- js/script.js
+++ js/script.js
@@ -1,5 +1,39 @@
 // coleta de dados para o formulario
+let projectNameInput = document.querySelector('#projectName');
+let budgetTypeInput = document.querySelector('#budgetType');
+let budgetTotalInput = document.querySelector('#budgetTotal');
+let budgetLeftInput = document.querySelector('#budgetLeft');
+let currencySelectInput = document.querySelector('#currencySelect')
+let purchaseCostInput = document.querySelector('#purchaseCost')
+
+// resposta
+let purchaseMotive = document.querySelector('#purchaseMotive')
+let btnCopy = document.querySelector('#btnCopy')
+let copyIcon = btnCopy.querySelector('.inputIcon')
+let btnText = btnCopy.querySelector('.btnText')
+
+function copyText() {
+
+    console.log(purchaseMotive.textContent)
+
+    try {
+        
+        navigator.clipboard.writeText(purchaseMotive.textContent)
+
+        copyIcon.src = 'img/icons/check.svg'
+        btnText.textContent = "COPIADO"
+
+    } catch (err) {
+
+        console.log(`Erro ao copiar texto: ${err}`)
+
+    }
+
+}
 
 
+btnCopy.addEventListener('click', copyText)
 
-export const totalUtilizado = 70;
+
+// PREENCHENDO GRAFICO
+export const totalUtilizado = 70
